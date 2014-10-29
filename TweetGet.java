78d086170644b09e8eff2bf0035ddde12721dee1
= <kmw2168@columbia.edu>
2014-10-29 17:00:51 -0400

    Tweet getter and DAO base classes

---
+++ TweetGet.java
@@ -0,0 +1,89 @@
+import twitter4j.GeoLocation;
+import twitter4j.StallWarning;
+import twitter4j.Status;
+import twitter4j.StatusDeletionNotice;
+import twitter4j.StatusListener;
+import twitter4j.TwitterException;
+import twitter4j.TwitterStream;
+import twitter4j.TwitterStreamFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * <p>This is a code example of Twitter4J Streaming API - sample method support.<br>
+ * Usage: java twitter4j.examples.PrintSampleStream<br>
+ * </p>
+ *
+ * @author Yusuke Yamamoto - yusuke at mac.com
+ */
+public final class TweetGet {
+
+	private static String oAuthConsumerKey = "oQ7aCFiY7cjfmLUutJiouvzw5";
+	private static String oAuthConsumerSecret = "mturYGKTi7CXhRlK9gkSJWF8XKyV1pTRLX7n2OBBydYKBTL9e6";
+	private static String oAuthAccessToken = "1952751391-ISOlpkQMwv79EOtUQRwdOhlmY5eZMCWM3TePX50";
+	private static String oAuthAccessTokenSecret = "lKbVowBHd7xogIsmdiuHB6WBGO0GyR8RoddDmS0XW0TGl";
+
+    /**
+     * Main entry of this application.
+     *
+     * @param args
+     */
+    public static void main(String[] args) throws TwitterException {
+    	 ConfigurationBuilder cb = new ConfigurationBuilder();
+         cb.setDebugEnabled(true)
+           .setOAuthConsumerKey(oAuthConsumerKey)
+           .setOAuthConsumerSecret(oAuthConsumerSecret)
+           .setOAuthAccessToken(oAuthAccessToken)
+           .setOAuthAccessTokenSecret(oAuthAccessTokenSecret);
+         
+        TwitterStream twitterStream = new TwitterStreamFactory(cb.build()).getInstance();
+        StatusListener listener = new StatusListener() {
+            @Override
+            public void onStatus(Status status) {
+                System.out.println("@" + status.getUser().getScreenName() + " - " + status.getText());
+                long statusId = status.getId();
+                String screenName = status.getUser().getScreenName();
+                String text = status.getText();
+                GeoLocation location = status.getGeoLocation();
+                double latitude = location.getLatitude();
+                double longitude = location.getLongitude();
+                String userProfileLocation = status.getUser().getLocation();  
+                
+                //TODO parse for keywords
+                //TODO if location == null, use userProfileLocation, and query google maps 
+                
+            }
+
+            @Override
+            public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
+                System.out.println("Got a status deletion notice id:" + statusDeletionNotice.getStatusId());
+                long statusId = statusDeletionNotice.getStatusId() //same ID as the status ID above - just delete from DB
+            }
+
+            @Override
+            public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
+                System.out.println("Got track limitation notice:" + numberOfLimitedStatuses);
+            }
+
+            @Override
+            public void onScrubGeo(long userId, long upToStatusId) {
+                System.out.println("Got scrub_geo event userId:" + userId + " upToStatusId:" + upToStatusId);
+            }
+
+            @Override
+            public void onStallWarning(StallWarning warning) {
+                System.out.println("Got stall warning:" + warning);
+            }
+
+            @Override
+            public void onException(Exception ex) {
+                ex.printStackTrace();
+            }
+        };
+        twitterStream.addListener(listener);
+        twitterStream.sample();
+    }
+}
---
+++ TwitterDAO.java
@@ -0,0 +1,94 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+
+public class TwitterDAO {
+	
+	private String insertSQL = "INSERT INTO Statuses(StatusId, ScreenName, Test, Latitude, Longitude, Keyword) " +
+								"VALUES (";
+	private String deleteSQL = "DELETE FROM Statuses WHERE StatusId = ";
+	private String endSQL = ")";
+	
+	public TwitterDAO() {
+		try {
+    		Class.forName("com.mysql.jdbc.Driver").newInstance();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+	}
+	
+	public void insertStatus(String statusId, String screenName, String text, long latitude, long longitude, List<String> keywords) {
+		Connection conn = null;
+		Statement stmt = null;
+    	try {
+    		conn = DriverManager.getConnection("jdbc:mysql://localhost/test?user=admin&password=assignment1rootpassword");
+    		stmt = conn.createStatement();
+    		String insertString;
+    		for (String keyword : keywords) {
+    			insertString = insertSQL;
+    			insertString += "'" + statusId + "'";
+    			insertString += "'" + screenName + "'";
+    			insertString += "'" + text + "'";
+    			insertString += "'" + latitude + "'";
+    			insertString += "'" + longitude + "'";
+    			insertString += "'" + keyword + "'";
+    			insertString += endSQL;
+    			stmt.executeUpdate(insertString);
+    		}
+    	} catch (SQLException e) {
+    		System.out.println("SQLException: " + e.getMessage());
+    	    System.out.println("SQLState: " + e.getSQLState());
+    	    System.out.println("VendorError: " + e.getErrorCode());
+    	} finally {
+    		try {
+		         if(stmt!=null) {
+		        	 conn.close();
+		         }
+    		} catch(SQLException se) {
+    			
+    		}
+    		try{
+    			if(conn!=null) {
+    				conn.close();
+    			}
+    		} catch(SQLException se) {
+    			se.printStackTrace();
+    		}
+    	}
+	}
+	
+	public void deleteStatus(String statusId) {
+		Connection conn = null;
+		Statement stmt = null;
+    	try {
+    		conn = DriverManager.getConnection("jdbc:mysql://localhost/test?user=admin&password=assignment1rootpassword");
+    		stmt = conn.createStatement();
+    		String deleteString = deleteSQL;
+    		deleteString += "'" + statusId + "'";
+    		deleteString += endSQL;
+    		stmt.executeUpdate(deleteString);
+    	} catch (SQLException e) {
+    		System.out.println("SQLException: " + e.getMessage());
+    	    System.out.println("SQLState: " + e.getSQLState());
+    	    System.out.println("VendorError: " + e.getErrorCode());
+    	} finally {
+    		try {
+		         if(stmt!=null) {
+		        	 conn.close();
+		         }
+    		} catch(SQLException se) {
+    			
+    		}
+    		try{
+    			if(conn!=null) {
+    				conn.close();
+    			}
+    		} catch(SQLException se) {
+    			se.printStackTrace();
+    		}
+    	}
+	}
+}
